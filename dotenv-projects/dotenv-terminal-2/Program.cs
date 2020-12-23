3cc5fb380a0c221d66224d170edb0cef02b0bc80
gustavoghp87 <ghp.2120@gmail.com>
2020-12-23 11:21:21 -0300

    varios

---
+++ dotenv-projects/dotenv-terminal-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+
+namespace dotenv_terminal_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string API_URL = "https://jsonplaceholder.typicode.com/posts?_limit=5";
+            var client = new WebClient();
+            var json = client.DownloadString(API_URL);
+            // Console.WriteLine(json);
+
+            dynamic posts = JsonConvert.DeserializeObject(json);
+            foreach(var post in posts) {
+                Console.WriteLine(post.id + ": " + post.title);
+            }
+
+        }
+    }
+}
