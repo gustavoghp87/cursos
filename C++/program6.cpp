d9526b42135a8fa8db532bb8c5cbdf68a5edb71b
Gustavo Hernán Perrupato <ghp.2120@gmail.com>
2021-09-21 22:14:46 -0300

    8812au-drivers, c++

--- C++/program6.cpp
+++ C++/program6.cpp
@@ -6,8 +6,31 @@ using namespace std;
 // it's possible to override external variables through memory directions
 // by this way, it is possible the multiple returning
 
+int matrix[10][10];
 void newValue(int&, int&);
 void toSquare(int vector[], int TAM);
+void printMatrix();
+void chargeMatrix();
+
+int main() {
+    int num1, num2;                   // these are internal variables
+    cout<<"Insert two numbers: ";
+    cin>>num1>>num2;
+    newValue(num1, num2);
+    cout<<"New first value is: "<<num1<<endl;
+    cout<<"New second value is: "<<num2<<endl;
+
+    const int LEN = 5;
+    int vector[LEN] = { 1, 2, 3, 4, 5 };
+    toSquare(vector, LEN);
+    // now vector is { 1, 4, 9, 16, 25 }
+    // the new array is not return from the function because C++ passes the array like a pointer and modifies the reference
+
+    chargeMatrix();
+    printMatrix();
+    getch();
+    return 0;
+}
 
 void newValue(int& num1, int& num2) {
     cout<<"First value is: "<<num1<<endl;
@@ -25,20 +48,19 @@ void toSquare(int vector[], int LEN) {
     }
 }
 
-int main() {
-    int num1, num2;                   // these are internal variables
-    cout<<"Insert two numbers: ";
-    cin>>num1>>num2;
-    newValue(num1, num2);
-    cout<<"New first value is: "<<num1<<endl;
-    cout<<"New second value is: "<<num2<<endl;
+void chargeMatrix() {
+    for (int i = 0; i<4; i++) {
+        for (int j = 0; j<5; j++) {
+            matrix[i][j] = i + j*10;
+        }
+    }
+}
 
-    const int LEN = 5;
-    int vector[LEN] = { 1, 2, 3, 4, 5 };
-    toSquare(vector, LEN);
-    // now vector is { 1, 4, 9, 16, 25 }
-    // the new array is not return from the function because C++ passes the array like a pointer and modifies the reference
-
-    getch();
-    return 0;
+void printMatrix() {
+    for (int i = 0; i<4; i++) {
+        for (int j = 0; j<5; j++) {
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
 }
